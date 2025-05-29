7e3a4607a30bfbf2855bf618ab4c14082414648a
AkashYdv07 <jaiakash2103@gmail.com>
2025-05-29 22:02:05 +0530

    Conditional stmt and Loops

---
+++ Loops_2/Conditional.java
@@ -0,0 +1,15 @@
+// conditional statement is used when we want either this or that 
+// there is a compulsion to have if you want else 
+public class Conditional {
+    public static void main(String[] args) {
+        int x = 8;
+        if(x>10)
+        {
+            System.out.println("Hello");
+        }
+        else 
+        {
+            System.out.println("Bye");
+        }
+    }
+}
