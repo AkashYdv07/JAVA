f87a8111bc90b2634bb963b96f26089319de3ac1
AkashYdv07 <jaiakash2103@gmail.com>
2025-09-13 23:48:56 +0530

    Completed Strings

---
+++ Strings_5/Example.java
@@ -0,0 +1,9 @@
+// String is not a datatype but it is a class
+// But in string we dont require new keyword as if we required it in creating oject
+//
+public class Example {
+    public static void main(String[] args) {
+        String name = new String(); // create a string variable/object
+        String str = "Hello World";
+    }
+}
